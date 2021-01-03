94c7e63f6ae4dbed3f194b2b0f4e539fafb4a241
SpensDee <71296101+SpensDee@users.noreply.github.com>
2021-01-04 00:53:14 +0200

    new

--- js/main.js
+++ js/main.js
@@ -2,4 +2,13 @@ $(function(){
     $('.blog__slider').slick({
        dots: true
     });
+
+    const mixer = mixitup('.gallery__items', {
+        animation: {
+            duration: 938,
+            nudge: true,
+            reverseOut: false,
+            effects: "fade translateZ(-15px) rotateX(-90deg) rotateY(-90deg)"
+        }
+    });
 });
